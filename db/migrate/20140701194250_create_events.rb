99dfa419877a7bdb67aaf82f92b7ee1691ca2c0d
chloeunrau <chloe.unrau@gmail.com>
2014-07-01 21:29:49 -0700

    Update event controller

--- db/migrate/20140701194250_create_events.rb
+++ db/migrate/20140701194250_create_events.rb
@@ -3,6 +3,7 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :game_title
       t.string :session_title
+      t.string :event_image
       t.datetime :date_time
       t.string :address
       t.string :instructions
