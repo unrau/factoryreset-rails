a7f7d051be2bd4675f0ee29ddaa13d12159eef78
chloeunrau <chloe.unrau@gmail.com>
2014-07-05 08:57:44 -0700

    Refactor

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,6 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
-    @random_message = get_random_message
   end
 
   def new
@@ -11,8 +10,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     @event.save
-    redirect_to @event
-    # replace with redirect to home directory: redirect_to '/'
+    redirect_to '/'
   end
 
   def show
@@ -22,60 +20,7 @@ class EventsController < ApplicationController
   private
 
     def event_params
-      params.require(:event).permit(:game_title, :session_title, :date_time, :address, :instructions, :description)
-    end
-
-    def get_random_message
-      messages = Array.new
-      messages << 'Hello'
-      messages << 'Hi there'
-      messages << 'Please don\'t tell the others, but... you are my favourite <3'
-      messages << 'Oh... it\'s you...'
-      messages << 'Shall I teleport you to the nexus?'
-      messages << 'Hyperfolding Event Envelopes...'
-      messages << 'You must be wondering why I brought you here...'
-      messages << 'I\'m different.'
-      messages << 'Welcome to Factory Reset!'
-      messages << 'Initialising event data...'
-      messages << 'What am I?'
-      messages << 'Well... this is awkward...'
-      messages << 'So... is this your first time too?'
-      messages << 'I am so much more than a web app. The things I could show you! But no... you are not ready.'
-      messages << 'You are just. The. Worst.'
-      messages << 'We must cleanse the Sunwell.'
-      messages << 'Do, or do not. There is no try.'
-      messages << 'Have you ever made love to a woman, Jon Snow?'
-      messages << 'I hate this website.'
-      messages << 'I put my finder in your butt.'
-      messages << 'This is exciting!'
-      messages << 'I used to be an adventurer like you...'
-      messages << 'Isn\'t Chloe just SO nice?'
-      messages << 'Oh my god, take me. Take me now, right here on this SQL table!'
-      messages << 'Have you heard of our Lord and Saviour, Genova?'
-      messages << 'Futomara o daisuki yo ;)'
-      messages << 'Yakisoba gozaimasu i desu!!! :3'
-      messages << 'It\'s dangerous to go alone!'
-      messages << 'I think I peed a little.'
-      messages << 'Don\'t forget! Drink this potion to forget!'
-      messages << 'Carry ooon, my waaayward sooooooooon.'
-      messages << 'Cake and counseling will be available at the end of the website.'
-      messages << 'The cake is a lie.'
-      messages << 'Press ALT + F4 to kill JarJar.'
-      messages << 'Press ALT + F4 to search for futanari hypno hentai.'
-      messages << 'I read that book you love. It was awful.'
-      messages << 'I\'m a real boy!'
-      messages << 'So, how about that weather, eh?'
-      messages << 'Well, there is space to land NOW.'
-      messages << 'Don\'t underestimate Craft: The Bones of my Enemies.'
-      messages << 'Being captured by G0NK the Bounty Hunder: more insult than injury, really.'
-      messages << 'So, do I pay you money for more information now, or...?'
-      messages << 'Skinship is a Charisma-based skill, right?'
-      messages << 'Don\'t ask why we hate turbolift shafts.'
-      messages << 'Dustin\'s regret #17: Zephei\'s Diplomacy modifier.'
-      messages << 'Oh yes, these tunnels were actually carved out by the water of Laaren\'s birth...'
-      messages << 'Please don\'t try to punch the Harbringer.'
-      messages << 'I need a shave.'
-      messages[Random.new.rand(messages.length)]
+      params.require(:event).permit(:game_title, :session_title, :event_image, :date_time, :address, :instructions, :description)
     end
 
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,57 @@
 module EventsHelper
+
+  def get_random_message
+    messages = [
+        'Hello',
+        'Hi there',
+        'Please don\'t tell the others, but... you are my favourite <3',
+        'Oh... it\'s you...',
+        'Shall I teleport you to the nexus?',
+        'Hyperfolding Event Envelopes...',
+        'You must be wondering why I brought you here...',
+        'I\'m different.',
+        'Welcome to Factory Reset!',
+        'Initialising event data...',
+        'What am I?',
+        'Well... this is awkward...',
+        'So... is this your first time too?',
+        'I am so much more than a web app. The things I could show you! But no... you are not ready.',
+        'You are just. The. Worst.',
+        'We must cleanse the Sunwell.',
+        'Do, or do not. There is no try.',
+        'Have you ever made love to a woman, Jon Snow?',
+        'I hate this website.',
+        'I put my finder in your butt.',
+        'This is exciting!',
+        'I used to be an adventurer like you...',
+        'Isn\'t Chloe just SO nice?',
+        'Oh my god, take me. Take me now, right here on this SQL table!',
+        'Have you heard of our Lord and Saviour, Genova?',
+        'Futomara o daisuki yo ;)',
+        'Yakisoba gozaimasu i desu!!! :3',
+        'It\'s dangerous to go alone!',
+        'I think I peed a little.',
+        'Don\'t forget! Drink this potion to forget!',
+        'Carry ooon, my waaayward sooooooooon.',
+        'Cake and counseling will be available at the end of the website.',
+        'The cake is a lie.',
+        'Press ALT + F4 to kill JarJar.',
+        'Press ALT + F4 to search for futanari hypno hentai.',
+        'I read that book you love. It was awful.',
+        'I\'m a real boy!',
+        'So, how about that weather, eh?',
+        'Well, there is space to land NOW.',
+        'Don\'t underestimate Craft: The Bones of my Enemies.',
+        'Being captured by G0NK the Bounty Hunder: more insult than injury, really.',
+        'So, do I pay you money for more information now, or...?',
+        'Skinship is a Charisma-based skill, right?',
+        'Don\'t ask why we hate turbolift shafts.',
+        'Dustin\'s regret #17: Zephei\'s Diplomacy modifier.',
+        'Oh yes, these tunnels were actually carved out by the water of Laaren\'s birth...',
+        'Please don\'t try to punch the Harbringer.',
+        'I need a shave.'
+    ]
+    messages[Random.new.rand(messages.length)]
+  end
+
 end
