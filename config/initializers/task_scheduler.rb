2ad403e82b8ac8de3f0d6fd81d3d21606cff1486
chloeunrau <chloe.unrau@gmail.com>
2014-07-15 22:12:29 -0700

    Change the SMS scheduler to send a reminder 4 hours before an event, instead of at a specified time

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -19,14 +19,17 @@ scheduler.cron '0 9 * * * America/Vancouver' do
   end
 end
 
-# Send reminder SMS messages every day at 12:00 noon PST
-# TODO: Send this SMS 4 hours before the event, instead of at a set time.
-scheduler.cron '0 12 * * * America/Vancouver' do
+# Send reminder SMS messages 4 hours before an event
+scheduler.each ('1m') do
   @events = Event.upcoming
   @events.each do |event|
     if Date.today.beginning_of_day == event.date_time.beginning_of_day
-      event.rsvps.each do |rsvp|
-        blowerio['/messages'].post :to => "+1#{rsvp.user.telephone.gsub!('.','')}", :message => "See you at #{event.address_title} for #{event.game_title} at #{event.date_time.strftime('%l:%M %p')}! :3"
+      event_time = event.date_time
+      in_four_hours = DateTime.now + 4.hours
+      if event_time.hour == in_four_hours.hour && event_time.min == in_four_hours.min
+        event.rsvps.each do |rsvp|
+          blowerio['/messages'].post :to => "+1#{rsvp.user.telephone.gsub!('.','')}", :message => "See you at #{event.address_title} for #{event.game_title} at #{event.date_time.strftime('%l:%M %p')}! :3"
+        end
       end
     end
   end
