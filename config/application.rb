8fed464fd108995fd154ddcbbf54db9be341dee6
chloeunrau <chloe.unrau@gmail.com>
2014-07-15 21:53:52 -0700

    Change the default timezone for active_record to America/Vancouver

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module Factoryreset
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     I18n.enforce_available_locales = true
+
+    config.time_zone = 'America/Vancouver'
+    config.active_record.default_timezone = :local
   end
 end
