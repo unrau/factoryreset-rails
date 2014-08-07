9aff70147a9f195a381047e83dbdec1b0dd0a781
chloeunrau <chloe.unrau@gmail.com>
2014-08-06 23:54:10 -0600

    Fix a typo

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -21,7 +21,7 @@ module EventsHelper
         'Do, or do not. There is no try.',
         'Have you ever made love to a woman, Jon Snow?',
         'This website sucks.',
-        'I put my finder in your butt.',
+        'I put my finger in your butt.',
         'This is exciting!',
         'I used to be an adventurer like you...',
         'Isn\'t Chloe just SO nice?',
