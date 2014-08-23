e48696c9064537a9751611e0e1d876fd1758b916
chloeunrau <chloe.unrau@gmail.com>
2014-08-23 10:49:22 -0700

    Refactor General Chat timeframe

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,8 +1,8 @@
 class Chat < ActiveRecord::Base
   belongs_to :user
 
-  time_from_now = Time.zone.now + 1.week
+  old_time = Time.zone.now - 1.week
 
   default_scope { order(created_at: :asc) }
-  scope :one_week_young, -> { all.where('created_at <= ?', time_from_now).order(created_at: :asc) }
+  scope :one_week_young, -> { all.where('created_at <= ?', old_time).order(created_at: :asc) }
 end
